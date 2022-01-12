2889ad8d6b8fbdb8df9c552bf497a97b6de138dc
princekalra <kalraprince75@gmail.com>
2022-01-12 13:08:29 +0530

    day-12

---
+++ Insert into a Binary Search Tree.cpp
@@ -0,0 +1,16 @@
+//Insert into a Binary Search Tree
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){
+            return new TreeNode(val);
+        }
+        if(val>root->val){
+            root->right = insertIntoBST(root->right,val);
+        }
+        else{
+            root->left = insertIntoBST(root->left,val);
+        }
+        return root;
+    }
+};
